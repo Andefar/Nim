222e1e9e96b16a6493e521b972c285992d60d92c
andefar40 <andreas.david.lauritzen@gmail.com>
2016-01-04 19:39:03 +0100

    lol

--- Gui.fs
+++ Gui.fs
@@ -4,29 +4,45 @@ module Gui
 open System
 open System.Drawing
 open System.Windows.Forms
+open System.Windows.Forms
 
     // The window part
 let window =
-  new Form(Text="Game of Nim", Size=Size(525,250))
+  new Form(Text="Game of Nim", Size=Size(525,300))
+
+let messages =
+  new Label(Location=Point(60,20),Size=Size(400,25),Text="")
+
+let matchDesc =
+  new Label(Location=Point(226,50),Size=Size(100,25),Text="Input here:")
 
 let input =
-  new TextBox(Location=Point(225,25),Size=Size(50,25))
+  new TextBox(Location=Point(227,75),Size=Size(50,25))
 
-let newGame =
-  new Button(Location=Point(200,160),MinimumSize=Size(100,50),
-              MaximumSize=Size(100,50),Text="Start New Game")
+let heap1Desc =
+  new Label(Location=Point(83,135),Size=Size(100,25),Text="Heap 1:")
+
+let heap2Desc =
+  new Label(Location=Point(233,135),Size=Size(100,25),Text="Heap 2:")
+
+let heap3Desc =
+  new Label(Location=Point(383,135),Size=Size(100,25),Text="Heap 3:")
 
 let heap1 =
-  new Button(Location=Point(50,65),MinimumSize=Size(100,50),
-              MaximumSize=Size(100,50),Text="4")
+  new Button(Location=Point(50,160),MinimumSize=Size(100,40),
+              MaximumSize=Size(100,50),Text="")
 
 let heap2 =
-  new Button(Location=Point(200,65),MinimumSize=Size(100,50),
-              MaximumSize=Size(100,50),Text="4")
+  new Button(Location=Point(200,160),MinimumSize=Size(100,40),
+              MaximumSize=Size(100,50),Text="")
 
 let heap3 =
-  new Button(Location=Point(350,65),MinimumSize=Size(100,50),
-              MaximumSize=Size(100,50),Text="4")
+  new Button(Location=Point(350,160),MinimumSize=Size(100,40),
+              MaximumSize=Size(100,50),Text="")
+
+let newGame =
+  new Button(Location=Point(200,220),MinimumSize=Size(100,50),
+              MaximumSize=Size(100,50),Text="Start New Game")
 
 let disable bs = 
     for b in [heap1;heap2;heap2] do 
@@ -34,29 +50,37 @@ let disable bs =
     for (b:Button) in bs do  
         b.Enabled  <- false
 
-let updateHeap n heap = 
+let enable bs = 
+    for (b:Button) in bs do 
+        b.Enabled  <- false
+    for (b:Button) in bs do 
+        b.Enabled  <- true
+
+
+let updateHeap heap (n:int) = 
     match heap with
-    | '1' -> (heap1.Text <- string(n))
-    | '2' -> (heap2.Text <- string(n))
-    | '3' -> (heap3.Text <- string(n))
+    | "1" -> (heap1.Text <- string(n))
+    | "2" -> (heap2.Text <- string(n))
+    | "3" -> (heap3.Text <- string(n))
     | _       -> failwith "heap does not exist"
 
+let printMessage str = 
+    messages.Text <- str
+    messages.AutoSize <- false
+    messages.TextAlign <- ContentAlignment.MiddleCenter 
+
+let showInput str = 
+    input.Text <- str
+
 let initGui = 
+    window.Controls.Add messages
+    window.Controls.Add matchDesc
     window.Controls.Add input
+    window.Controls.Add heap1Desc
+    window.Controls.Add heap2Desc
+    window.Controls.Add heap3Desc
     window.Controls.Add heap1
     window.Controls.Add heap2
     window.Controls.Add heap3
-    //This is tests. Eventlisteners must call nim-model functions and not gui functions
-//    Application.Run(window)
+    window.Controls.Add newGame
 
-//
-//window.Controls.Add input
-//window.Controls.Add heap1
-//window.Controls.Add heap2
-//window.Controls.Add heap3
-//window.Controls.Add newGame
-//heap1.Click.Add (fun _ -> updateHeap input.Text '1')
-//heap2.Click.Add (fun _ -> updateHeap input.Text '2')
-//heap3.Click.Add (fun _ -> updateHeap input.Text '3')
-//
-//Application.Run(window) 
--- Nim.fsx
+++ Nim.fsx
@@ -12,6 +12,13 @@ open System.Threading
 open System.Windows.Forms 
 open System.Drawing 
 
+let printCollection msg coll =
+        printfn "%s:" msg
+        Seq.iteri (fun index item -> printfn "  %i: %O" index item) coll
+
+
+
+
 // An asynchronous event queue kindly provided by Don Syme 
 type AsyncEventQueue<'T>() = 
     let mutable cont = None 
@@ -37,67 +44,131 @@ type AsyncEventQueue<'T>() =
 
 
 // Automaton, Controller
-type heap = int * char
+type heap = string * int
 type Message = 
-    | Input of heap | Clear | Cancel | IllegalInput| Legal
+    | Input of heap | Clear | Cancel | IllegalInput | Legal | Restart
+
+let restartMap = 
+    let heap1 = ("1",10)
+    let heap2 = ("2",10)
+    let heap3 = ("3",10)
+    Map.ofList [heap1;heap2;heap3]
+
+let mutable heaps = restartMap
 
 
-let input = 0;;
-let heap1 = (10,'1');;
-let heap2 = (10,'2');;
-let heap3 = (10,'3');;
+let updateHeapsMap h removed =
+    Map.add h removed (Map.filter (fun k v -> k<>h) heaps)
 
-let heaps = [heap1;heap2;heap3]
+let checkValidInputAndMove h i map  = 
+    let curMatches = (Map.find h heaps) - i
+
+    if (curMatches >= 0 && i >= 0) then 
+         Gui.updateHeap h curMatches
+         heaps <- updateHeapsMap h curMatches
+         Legal
+     else
+        Gui.printMessage "Input is invalid :P go again.."
+        IllegalInput
+
+
+let moveComp heapsmap = 
+    Gui.printMessage "Now the computer will make its move! xD"
+    let m = Map.fold (fun state _ value -> state ^^^ value) 0 heapsmap
+    if(m <> 0) then
+        let ak_xorb_m_key =  (Map.findKey (fun key value -> value ^^^ m < value) heapsmap)
+        let ak_xorb_m = Map.find ak_xorb_m_key heapsmap
+        let sub = ak_xorb_m ^^^ m
+        let diff = ak_xorb_m - sub
+        Gui.showInput diff.ToString
+        Thread.Sleep(2000)
+        Gui.updateHeap h curMatches
+        heaps <- updateHeapsMap h curMatches
+
+    else
+
+
+
+let mutable first = true
+let mutable playersTurn = true
+
 let eventQ = AsyncEventQueue()
-
 let rec initGame() =
-    async{printf("Init")
-          List.iter (fun (i,n) -> Gui.updateHeap i n) heaps
-          
+    async{//printf("Init\n")
+          Gui.printMessage "Welcome! Enter number of matches you want to remove and select a heap"
+          Gui.enable [Gui.heap1;Gui.heap2;Gui.heap3];
+          heaps <- restartMap
+          first <- true
+          Map.iter (fun key value -> Gui.updateHeap key value) heaps
+          Gui.disable [Gui.newGame]
           return! ready()}
 
 and ready() = 
-    async {printf("Ready")
-           Gui.disable [Gui.newGame]
-
+    async {//printf("Ready\n")
+           if not first then Gui.printMessage "Great move! ;)"
            let! input = eventQ.Receive()
            match input with
-           | Input (i,h)    -> return! checkInput i h 
+           | Input (h,i)    -> return! checkInput h i 
            | Clear          -> return! ready()
            | _              -> failwith("Ready: You fool")}
 
-and checkInput i h = 
-    async {printf("checkInput") 
-           Gui.updateHeap i h
-           
-           // TODO: Check input
-           return! checkStatus()}
+and checkInput h i = 
+    async {//printf("checkInput\n")
+           first <- false
+           playersTurn <- false
+           let input = checkValidInputAndMove h i heaps
+           match input with
+           | Legal          -> return! checkStatus()
+           | IllegalInput   -> return! ready()
+           | _              -> failwith "checkinput failed"}
+
+
 
 and checkStatus() = 
-    async {printf("checkStatus")
-           return! ready()
-//           if (List.forall (fun x -> x=0) [n1;n2;n3]) then finished()
-//           else computer()
+    async {//printf("checkStatus\n")
+           printCollection "heaps" heaps
+           if (Map.forall (fun _ value -> value = 0) heaps) then return! finished()
+           elif playersTurn then return! ready()
+           else return! computer()
            }
 
-//and finished() = 
-//    async {initGame()
-//           return! ready()}
-
-//and computer() = 
-//    async {Gui.UpdateHeap n1 heap1
-//           Gui.UpdateHeap n2 heap2
-//           Gui.UpdateHeap n3 heap3
-//           
-//           return! ready()}
+and finished() = 
+    async {//printf("finished\n")
+           Gui.printMessage "xxxxxx WON! :D wanna try again? hit the \"Start New Game\""
+           Gui.enable [Gui.newGame]
+           Gui.disable [Gui.heap1;Gui.heap2;Gui.heap3];
+           let! input = eventQ.Receive()
+           match input with
+           | Restart    -> return! initGame()
+           | _          -> failwith("finished: You fool")}
+          
+and computer() = 
+    async {//TODO: implement computers algorithm in movecomp function (see above)
+           playersTurn <- true
+           let input = moveComp heaps
+           match input with
+           | Legal    -> return! checkStatus()
+           | _        -> failwith("computers move fail: You fool")}
 
 let addListeners = 
-    Gui.heap1.Click.Add (fun _ -> eventQ.Post (Input (int(Gui.input.Text),'1')))
-    Gui.heap2.Click.Add (fun _ -> eventQ.Post (Input (int(Gui.input.Text),'2')))
-    Gui.heap3.Click.Add (fun _ -> eventQ.Post (Input (int(Gui.input.Text),'3')))
+    Gui.heap1.Click.Add (fun _ -> eventQ.Post (Input ("1",int(Gui.input.Text))))
+    Gui.heap2.Click.Add (fun _ -> eventQ.Post (Input ("2",int(Gui.input.Text))))
+    Gui.heap3.Click.Add (fun _ -> eventQ.Post (Input ("3",int(Gui.input.Text))))
+    Gui.newGame.Click.Add (fun _ -> eventQ.Post Restart)
 
 let startGame =
     Gui.initGui
     addListeners
-    Gui.window.Show()
+    //Gui.window.Show()
     Async.StartImmediate (initGame())
+    Application.Run(Gui.window)
+
+ 8 ^^^ 4 ^^^ 3
+
+8 ^^^ 15
+(* M != 0 
+1. m = 15
+2. find ak_xorb_m ---> 8
+3. find ak_xorb_m_key ---> 1
+4. let sub = ak_xorb_m ^^^ m = 7
+5. let diff = ak_xorb_m - sub = 1
